df24bb93bf262c12c884d75d9799740e1b808c9b
Ierzi <ierziytb@gmail.com>
2025-08-20 21:27:50 +0200

    more cool reactions

--- reactions.py
+++ reactions.py
@@ -13,7 +13,7 @@ class Reactions(commands.Cog):
     @commands.command()
     async def kiss(self, ctx: commands.Context, user: discord.Member):
         if user == ctx.author:
-            await ctx.send(f"{ctx.author.mention} kisses themselves... that's a bit sad.")
+            await ctx.send(f"{ctx.author.mention} kisses themselves... that's a bit sad.", allowed_mentions=discord.AllowedMentions.none())
             return
         if user.id == self.bot.user.id:
             reaction = random.choice(["૮ ˶ᵔ ᵕ ᵔ˶ ა", ">/////<", "m- me? ._."])
@@ -23,7 +23,35 @@ class Reactions(commands.Cog):
             await ctx.send("ok what :broken_heart:")
             return
         
-        await ctx.send(f"{ctx.author.mention} kisses {user.mention} ❤️", allowed_mentions=discord.AllowedMentions(users=False))
+        await ctx.send(f"{ctx.author.mention} kisses {user.mention} ❤️", allowed_mentions=discord.AllowedMentions.none())
+    
+    @commands.command()
+    async def hug(self, ctx: commands.Context, user: discord.Member):
+        if user == ctx.author:
+            ctx.send(f"{ctx.author.mention} hugs themselves :sob: \n-#I'd argue that hugging a pillow is better.", allowed_mentions=discord.AllowedMentions.none())
+            return
+        if user.id == self.bot.user.id:
+            await ctx.send("Hey I needed that hug... thank you <333")
+            return
+        if user.bot:
+            await ctx.send("not only are you hugging a bot, but it's not even me? :pensive:")
+            return
+        await ctx.send(f"{ctx.author.mention} hugs {user.mention} 🤗", allowed_mentions=discord.AllowedMentions.none())
+    
+    @commands.command()
+    async def slap(self, ctx: commands.Context, user: discord.Member):
+        if user == ctx.author:
+            await ctx.send(f"this dumbass {ctx.author.mention} is slapping himself smh my head", allowed_mentions=discord.AllowedMentions.none())
+            return
+        if user.id == self.bot.user.id:
+            await ctx.send("fuck you")
+            return
+        if user.bot:
+            await ctx.send(f"{ctx.author.mention} **SLAPS** {user.mention}! \n-#deserved icl cause im better", allowed_mentions=discord.AllowedMentions.none())
+            return
+        
+        await ctx.send(f"{ctx.author.mention} **SLAPS** {user.mention}!", allowed_mentions=discord.AllowedMentions.none())
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(Reactions(bot))
