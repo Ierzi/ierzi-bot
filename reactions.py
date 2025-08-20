6006a2f9a1a14bd5084a06d1778941b1e4cf30c1
Ierzi <ierziytb@gmail.com>
2025-08-20 21:02:12 +0200

    personality

--- reactions.py
+++ reactions.py
@@ -1,6 +1,7 @@
 import discord
 from discord.ext import commands
 from rich.console import Console
+import random
 
 console = Console()
 
@@ -13,6 +14,13 @@ class Reactions(commands.Cog):
     async def kiss(self, ctx: commands.Context, user: discord.Member):
         if user == ctx.author:
             await ctx.send(f"{ctx.author.mention} kisses themselves... that's a bit sad.")
+            return
+        if user.id == self.bot.user.id:
+            reaction = random.choice(["૮ ˶ᵔ ᵕ ᵔ˶ ა", ">/////<", "m- me? ._."])
+            await ctx.send(reaction)
+            return
+        if user.bot:
+            await ctx.send("ok what :broken_heart:")
             return
         
         await ctx.send(f"{ctx.author.mention} kisses {user.mention} ❤️")
