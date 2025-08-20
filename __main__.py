4c408ce660d7ced3e9d1f6905556f93d208ddac7
Ierzi <ierziytb@gmail.com>
2025-08-20 10:54:23 +0200

    List marriages command

--- __main__.py
+++ __main__.py
@@ -28,8 +28,18 @@ async def hello(ctx: commands.Context):
     await ctx.send("Hello!")
 
 # Marriage commands
-async def add_marriage_list(marriage_pair):
+
+async def update_marriage_list():
+    with open("marriages.json", "r") as f:
+        global marriages
+        marriages = list(json.load(f))
+
+async def add_marriage_list(marriage_pair: tuple):
     """Update the marriage list with a new marriage pair."""
+    if marriage_pair in marriages:
+        console.print(f"Marriage pair {marriage_pair} already exists in the list.")
+        return
+    
     marriages.append(marriage_pair)
     marriages.append((marriage_pair[1], marriage_pair[0]))
 
@@ -59,7 +69,7 @@ async def marry(ctx: commands.Context, partner: discord.Member):
         await ctx.send("You are already married to this person!")
         return
     
-    await ctx.send(f"{partner.name}, do you want to marry {proposer.name}?", silent=True)
+    await ctx.send(f"{partner.mention}, do you want to marry {proposer.mention}?", allowed_mentions=discord.AllowedMentions.none())
     await ctx.send(f"Reply with yes if you accept, or no if you decline.")
     
     def check(m: discord.Message):
@@ -72,7 +82,7 @@ async def marry(ctx: commands.Context, partner: discord.Member):
         return
     
     if msg.content.lower() == "yes":
-        await ctx.send(f"Congratulations {proposer.name} and {partner.name}, you are now happilly married!", silent=True)
+        await ctx.send(f"Congratulations {proposer.mention} and {partner.mention}, you are now happilly married!", allowed_mentions=discord.AllowedMentions.none())
         console.print(f"Marriage between {proposer.name} and {partner.name} has been recorded.")
         await add_marriage_list((proposer.id, partner.id))
     else:
@@ -86,8 +96,8 @@ async def divorce(ctx: commands.Context, partner: discord.Member):
         await ctx.send("You are not married to this person!")
         return
     
-    await ctx.send(f"Are you sure you want to divorce {partner.name}?")
-    await ctx.send(f"Reply with yes if you accept, or no if you decline.")
+    await ctx.send(f"Are you sure you want to divorce {partner.mention}?", allowed_mentions=discord.AllowedMentions.none())
+    await ctx.send(f"Reply with yes if you confirm, or no if you decline.")
     
     def check(m: discord.Message):
         return m.author.id == proposer.id and m.channel == ctx.channel and m.content.lower() in ["yes", "no"]
@@ -99,11 +109,75 @@ async def divorce(ctx: commands.Context, partner: discord.Member):
         return
     
     if msg.content.lower() == "yes":
+        # Remove the marriage from the list
         await remove_marriage_list((proposer.id, partner.id))
-        await ctx.send(f"Divorce between {proposer.name} and {partner.name} has been processed.", silent=True)
+        await ctx.send(f"{proposer.mention} and {partner.mention} have been divorced.", allowed_mentions=discord.AllowedMentions.none())
+        await ctx.send("-# its over...")
         console.print(f"Divorce between {proposer.name} and {partner.name} has been recorded.")
     else:
         await ctx.send(f"{proposer.mention} has declined the divorce proposal.")
 
+
+# TODO
+@bot.command()
+async def listmarriages(ctx: commands.Context, page_number: int = 1):
+    """List all current marriages."""
+    # Update
+    await update_marriage_list()
+
+    if not marriages:
+        await ctx.send("Nobody is married yet!")
+        return
+    else:
+        if page_number > n_pages:
+            await ctx.send(f"Invalid page number. There are only {n_pages} pages of marriages.")
+            return
+        
+        # Send some sort of message to indicate that the bot is processing the request
+        think = await ctx.send("Thinking...")
+        think_slow = await ctx.send("-# slow ass program (why did i make it in python)")
+        # Calculate the number of pages based on the number of marriages
+        n_marriages = len(marriages) 
+        n_pages = round(n_marriages // 10 + 1)
+        
+        all_messages = ""
+        start_index = (page_number - 1) * 10
+        count = 0
+        mess_count = 0
+        for i, pair in enumerate(marriages):
+            count += 1
+            if count <= start_index:
+                continue
+            if mess_count == 10:
+                # Reach the end of the page
+                break
+
+            if i % 10 == 0:
+                all_messages += f"**Page {page_number}**\n"
+            
+            if i >= start_index:
+                user_1 = bot.get_user(pair[0]) or await bot.fetch_user(pair[0])
+                user_2 = bot.get_user(pair[1]) or await bot.fetch_user(pair[1])
+                all_messages += f"{user_1.mention} and {user_2.mention}\n"
+                mess_count += 1
+        
+        # After thinking, deletes the "Thinking..." message
+        await think.delete()
+        await think_slow.delete()
+        await ctx.send(all_messages, allowed_mentions=discord.AllowedMentions.none())
+
+@bot.command()
+async def debug(ctx: commands.Context, fake_n_marriages: int | None = None):
+    """Debug command to check bot status."""
+    if not fake_n_marriages:
+        n_marriages = len(marriages)
+        await ctx.send(f"Current marriages: {n_marriages} pairs.")
+        await ctx.send(marriages)
+    else:
+        n_marriages = fake_n_marriages
+        await ctx.send(f"Debugging with {n_marriages} fake marriages.")
+
+    number_of_pages = round(n_marriages // 10 + 1)
+    await ctx.send(number_of_pages)
 
 bot.run(token)
