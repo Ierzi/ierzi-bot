6fde1d3acd84d1a3fc0e1019d821995df14bbeed
Ierzi <ierziytb@gmail.com>
2025-08-23 22:17:33 +0200

    UPDATE THE WIKI

--- __main__.py
+++ __main__.py
@@ -87,7 +87,8 @@ async def roadmap(ctx: commands.Context):
     """features i wanna add"""
     features = [
         "debug thing with spendings ai", "fix !work", "add more reactions", 
-        "!listmarrriages",  "custom emojis", "song recommendation based on my playlist"
+        "!listmarrriages",  "custom emojis", "song recommendation based on my playlist",
+        "UPDATE THE WIKI"
     ] # the cat command is gonna piss off fact lmao
     message = "Features I wanna add: \n"
     for feature in features:
