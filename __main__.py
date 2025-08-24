7cd97bed0a92173cf19464bc75e985ee3c81fd73
Ierzi <ierziytb@gmail.com>
2025-08-24 21:24:21 +0200

    shower tim'

--- __main__.py
+++ __main__.py
@@ -88,8 +88,7 @@ async def roadmap(ctx: commands.Context):
     features = [
         "debug thing with spendings ai", "fix !work", "add more reactions", 
         "!listmarrriages",  "custom emojis", "song recommendation based on my playlist",
-        "UPDATE THE WIKI", "cat videos" "switch to openrouter"
-    ] 
+        "UPDATE THE WIKI", "cat videos", "switch to openrouter", "pay ppl"]
     message = "Features I wanna add: \n"
     for feature in features:
         message += f"- {feature}\n"
