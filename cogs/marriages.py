0e6cbba7ab39b105041c3bb8d95edea074b48f46
definitelytotallynotguest <hippopotomonstrosesquipedaliophobia.html+1@protonmail.com>
2025-09-05 18:54:09 -0300

    wahhh

--- cogs/marriages.py
+++ cogs/marriages.py
@@ -127,7 +127,12 @@ class Marriages(commands.Cog):
             self.console.print(f"Divorce between {proposer.name} and {partner.name} has been recorded.")
         else:
             await ctx.send(f"{proposer.mention} has canceled the divorce proposal.", allowed_mentions=discord.AllowedMentions.none())
-            
+
+        if user.id == 1206615811792576614:
+            await ctx.send("Not now big guy~")
+            return
+
+    
     # TODO: fix this
     # @commands.command()
     # async def listmarriages(self, ctx: commands.Context, page_number: int = 1):
