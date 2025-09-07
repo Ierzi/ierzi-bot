ad175cf9e00757b488eccc355a804c9a1dbb6ef4
Ierzi <ierziytb@gmail.com>
2025-09-07 16:09:14 +0200

    removed useless imports pt 2

--- __main__.py
+++ __main__.py
@@ -1,5 +1,5 @@
 import discord
-from discord import ButtonStyle, Interaction, Embed, Message
+from discord import ButtonStyle, Interaction, Embed
 from discord.ext import commands
 from discord.ui import Button, View
 from rich.console import Console
