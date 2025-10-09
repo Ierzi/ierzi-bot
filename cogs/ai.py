cd5e16bac4a0f5f3a2522c568d428a4d1c8a8e02
Ierzi <ierziytb@gmail.com>
2025-10-09 16:22:25 +0200

    fix

--- cogs/ai.py
+++ cogs/ai.py
@@ -194,7 +194,7 @@ class AI(commands.Cog):
                         s_json = await s.json()
                         status = s_json["status"]
                         self.console.print("Job status:", status)
-                        if status in ("succeeded", "failed", "cancelled"):
+                        if status in ("completed", "failed", "cancelled"):
                             self.console.print("Got status")
                             break
 
