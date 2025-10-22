518eb44b97476944f99e770986d544aa09783c23
Ierzi <105780029+Ierzi@users.noreply.github.com>
2025-10-22 17:02:52 +0200

    _

--- __main__.py
+++ __main__.py
@@ -584,7 +584,7 @@ async def get_pronouns(ctx: commands.Context, user: Optional[discord.User] = Non
             await ctx.send(f"{user_profile.mention} didn't set their pronouns.")
             return
         else:
-            if pronouns == 'fag/got' and guild_id == 980439011560661002: #eddgows server
+            if _pronouns == 'fag/got' and guild_id == 980439011560661002: #eddgows server
                 await ctx.send(f"{user_profile.mention}'s pronouns are a mf slur 💔")
                 return
             await ctx.send(f"{user_profile.mention}'s pronouns are {_pronouns}.")
@@ -593,7 +593,7 @@ async def get_pronouns(ctx: commands.Context, user: Optional[discord.User] = Non
             await ctx.send("You didn't set your pronouns! Use !pronouns to set them.")
             return
         else:
-            if pronouns == 'fag/got' and guild_id == 980439011560661002: #eddgows server
+            if _pronouns == 'fag/got' and guild_id == 980439011560661002: #eddgows server
                 await ctx.send(f"Your pronouns are a slur and daddy eddgow dont allow them here")
                 return
             await ctx.send(f"Your current pronouns are {_pronouns}.")
