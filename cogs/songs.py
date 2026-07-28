e03bd7e869abf06a9bfddbfa92a659f5b0ebd301
Ierzi <ierziytb@gmail.com>
2026-07-28 19:00:07 -0400

    debug 2

--- cogs/songs.py
+++ cogs/songs.py
@@ -555,7 +555,7 @@ class Songs(commands.Cog):
                         return
 
                     data = await response.json()
-                    await ctx.send(data)
+                    self.console.print(data)
                     track_info = data.get("track", {})
                     # await ctx.send(track_info if len(track_info) < 2000 else "track info too long")
                     if not track_info:
