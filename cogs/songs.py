714938fa57e2962089ee8dd75b60ad99ea2b84ae
Ierzi <ierziytb@gmail.com>
2026-07-28 19:25:01 -0400

    ??????

--- cogs/songs.py
+++ cogs/songs.py
@@ -555,6 +555,7 @@ class Songs(commands.Cog):
                         return
 
                     data = await response.json()
+                    self.console.print(data[:1]) # ????
                     track_info = data.get("track", {})
                     # await ctx.send(track_info if len(track_info) < 2000 else "track info too long")
                     if not track_info:
